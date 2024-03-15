ac0109a0ea5db9c054d6ca3785ad1ee69ffe7482
yoan <yoanyombapro@gmail.com>
2024-03-15 00:28:38 -0400

    feat: update workspace schema
    Signed-off-by: yoan <yoanyombapro@gmail.com>

--- openapi2krakend/pkg/models/defaults.go
+++ openapi2krakend/pkg/models/defaults.go
@@ -14,7 +14,7 @@ func DefaultEndpointExtraConfig(environment string) map[string]interface{} {
 				"cache":                true,
 				"cache_duration":       3600,
 				"alg":                  "RS256",
-				"audience":             []string{"https://solomon-ai.us.auth0.com/api/v2/"},
+				"audience":             []string{"https://solomon-ai.us.auth0.com/api/v2/", "https://app-platform.solomon-ai.io"},
 				"jwk_url":              "https://solomon-ai.us.auth0.com/.well-known/jwks.json",
 			},
 		}
@@ -32,7 +32,7 @@ func DefaultEndpointExtraConfig(environment string) map[string]interface{} {
 			"cache":                true,
 			"cache_duration":       3600,
 			"alg":                  "RS256",
-			"audience":             []string{"https://dev-26i1fsf2m1n6hqk1.us.auth0.com/api/v2/"},
+			"audience":             []string{"https://dev-26i1fsf2m1n6hqk1.us.auth0.com/api/v2/", "https://app-release.solomon-ai.io", "http://localhost:3000"},
 			"jwk_url":              "https://dev-26i1fsf2m1n6hqk1.us.auth0.com/.well-known/jwks.json",
 		},
 	}
